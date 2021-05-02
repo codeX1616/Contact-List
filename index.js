1134278b298eaa2035c7e5fde562ad06898d581d
pratik1616 <Pjoshi@mphrx.com>
2021-05-02 17:24:11 +0530

    Added title variable to be used by home.ejs

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const express = require('express');
-
-// Module to set path for view engine
 const path = require('path');
-
 const port = 8000;
 const app = express();
 
-// Adding ejs template/view engine
 app.set('view engine', 'ejs');
-
-// Setting path to add ejs
 app.set('views',path.join(__dirname, 'views'));
 
 app.get('/', function(req,res){
 
-    // res.render function renders the home.ejs file to the specified path
-    return res.render('home')
+    // Creating title variable to be used by home.ejs file when needed
+    return res.render('home', {title: 'Contacts List'});
 });
 
 app.listen(port, function(err){
